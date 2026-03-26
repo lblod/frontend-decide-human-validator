6cd19839db2bdee403fd9b964fd1101311a57654
karel kremer <karel.kremer@redpencil.io>
2026-03-26 12:44:33 +0100

    make validation buttons no longer mocked

--- app/components/validation-thumbs.js
+++ app/components/validation-thumbs.js
@@ -1,30 +1,65 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class ValidationThumbs extends Component {
-  // TODO stubbed, to be done in separate story
+  @tracked approveCount = undefined;
+  @tracked rejectCount = undefined;
+  @tracked ownReview = undefined;
+
   get approvedCount() {
-    return 10;
+    if (this.approveCount !== undefined) {
+      return this.approveCount;
+    }
+    return this.args.annotation.counts?.approve || 0;
   }
   get rejectedCount() {
-    return 12;
+    if (this.rejectCount !== undefined) {
+      return this.rejectCount;
+    }
+    return this.args.annotation.counts?.reject || 0;
   }
 
   get approved() {
-    return true;
+    if (this.ownReview !== undefined) {
+      return this.ownReview === 'approve';
+    }
+    return this.args.annotation.counts?.ownReview === 'approve';
   }
 
   get rejected() {
-    return false;
+    if (this.ownReview !== undefined) {
+      return this.ownReview === 'reject';
+    }
+    return this.args.annotation.counts?.ownReview === 'reject';
+  }
+
+  async updateAnnotationComponentState(annotationResponse) {
+    const counts = await annotationResponse.json();
+    this.approveCount = counts.approve || 0;
+    this.rejectCount = counts.reject || 0;
+    this.ownReview = counts.ownReview;
   }
 
   @action
-  approve() {
-    console.log('todo approve');
+  async approve() {
+    const response = await fetch(
+      `/annotation-review/review/${this.args.annotation.id}/approve`,
+      {
+        method: 'POST',
+      },
+    );
+    await this.updateAnnotationComponentState(response);
   }
 
   @action
-  reject() {
-    console.log('todo reject');
+  async reject() {
+    const response = await fetch(
+      `/annotation-review/review/${this.args.annotation.id}/reject`,
+      {
+        method: 'POST',
+      },
+    );
+    await this.updateAnnotationComponentState(response);
   }
 }
