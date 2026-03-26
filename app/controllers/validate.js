859456e21910baab743bb70fc33189307d6b6d8d
karel kremer <karel.kremer@redpencil.io>
2026-03-26 15:33:34 +0100

    allow opening annotation location

--- app/controllers/validate.js
+++ app/controllers/validate.js
@@ -1,8 +1,16 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class ValidateController extends Controller {
   queryParams = ['page', 'size'];
   @tracked page = 0;
   @tracked size = 8;
+
+  @tracked selectedAnnotation = null;
+
+  @action
+  selectAnnotation(annotation) {
+    this.selectedAnnotation = annotation;
+  }
 }
