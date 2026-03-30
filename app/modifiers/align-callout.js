f71ffc3c612aa04b03c13331f693cd3c78fc3e9a
karel kremer <karel.kremer@redpencil.io>
2026-03-30 16:12:46 +0200

    fix linting

--- app/modifiers/align-callout.js
+++ app/modifiers/align-callout.js
@@ -1,6 +1,6 @@
 import { modifier } from 'ember-modifier';
 
-export default modifier((element, [_annotation]) => {
+export default modifier((element) => {
   element.classList.remove('right');
   if (element.parentElement.offsetLeft > element.clientWidth) {
     element.classList.add('right');
