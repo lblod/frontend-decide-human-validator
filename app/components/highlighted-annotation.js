d816dd9d367654804a1b4e8e7616584a535f5bbb
karel kremer <karel.kremer@redpencil.io>
2026-04-13 11:26:47 +0200

    show map for location annotations

--- app/components/highlighted-annotation.js
+++ app/components/highlighted-annotation.js
@@ -1,9 +1,13 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { service } from '@ember/service';
 export default class HighlightedAnnotation extends Component {
+  @service store;
+
   @tracked
   showInfo = false;
+  @tracked geometry = null;
 
   get icon() {
     return 'eye';
@@ -16,13 +20,34 @@ export default class HighlightedAnnotation extends Component {
   }
 
   @action
-  resetShowInfo() {
+  async resetShowInfo() {
     this.showInfo = false;
+    if (this.args.annotation.type === 'http://purl.org/dc/terms/Location') {
+      const locations = await this.store.query('location', {
+        'filter[:uri:]': this.args.annotation.value,
+        include: 'geometry',
+      });
+      if (locations.length < 1) {
+        this.geometry = null;
+      } else {
+        this.geometry = await locations[0].geometry;
+      }
+    } else {
+      this.geometry = null;
+    }
   }
 
   @action
   toggleShowInfo(event) {
-    if (event.target.classList.contains('highlighted-annotation')) {
+    let maybeButton = event.target;
+    while (
+      maybeButton &&
+      !maybeButton.classList.contains('highlighted-annotation') &&
+      !maybeButton.classList.contains('highlight-info')
+    ) {
+      maybeButton = maybeButton.parentElement;
+    }
+    if (maybeButton?.classList.contains('highlighted-annotation')) {
       this.showInfo = !this.showInfo;
     }
   }
