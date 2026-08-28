9482f64cd72b38fe8e545b643b8ce9dcf3e0bb89
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-08-28 13:13:44 +0200

    feat: cache the options in the option service for the dropdowns

--- app/services/options.js
+++ app/services/options.js
@@ -1,24 +1,48 @@
-import Service from '@ember/service';
+import Service, { service } from '@ember/service';
 
 export default class OptionsService extends Service {
+  @service store;
+
+  _predicates = [];
+  _aiModels = [];
+  _valueTypes = [];
+
   async predicates() {
+    if (this._predicates?.length >= 1) {
+      return this._predicates;
+    }
+
     const response = await fetch(`/annotation-review/options/predicates`);
     const results = await response.json();
 
-    return results ?? [];
+    this._predicates = results ?? [];
+
+    return this._predicates;
   }
 
   async aiModels() {
+    if (this._aiModels?.length >= 1) {
+      return this._aiModels;
+    }
+
     const response = await fetch(`/annotation-review/options/ai-models`);
     const results = await response.json();
 
-    return results ?? [];
+    this._aiModels = results ?? [];
+
+    return this._aiModels;
   }
 
   async valueTypes() {
+    if (this._valueTypes?.length >= 1) {
+      return this._valueTypes;
+    }
+
     const response = await fetch(`/annotation-review/options/value-types`);
     const results = await response.json();
 
-    return results ?? [];
+    this._valueTypes = results ?? [];
+
+    return this._valueTypes;
   }
 }
